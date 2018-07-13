9a194c7ad1bc3d8e0736bee805a1e74880cf7ae6
Kristina Tong <kristina.tong@gmail.com>
2018-07-13 21:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber.length === 0){
+  if(cardNumber === undefined){
     return  'Sorry, we don\'t have a credit card on file for you.'
   }
   else{
