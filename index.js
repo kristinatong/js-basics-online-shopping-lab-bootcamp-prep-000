57d54e21e3c470b72721e2a492eba81b02d2d2d0
Kristina Tong <kristina.tong@gmail.com>
2018-07-13 21:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber === null){
-    return  'Sorry, we don\'t have a credit card on file for you.'
+    return  "Sorry, we don't have a credit card on file for you."
   }
   else{
     var total = total()
