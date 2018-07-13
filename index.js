a598dacd89a06229104a6912df64f82d5f3c84bc
Kristina Tong <kristina.tong@gmail.com>
2018-07-13 21:14:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,11 +65,12 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  total = total()
   if(cardNumber === null){
     return  'Sorry, we don\'t have a credit card on file for you.'
   }
   else{
     cart.splice(0)
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
   }
 }
