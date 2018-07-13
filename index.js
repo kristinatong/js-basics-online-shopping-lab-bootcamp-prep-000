03e5590b7491db49d804453fa613f8517a456f92
Kristina Tong <kristina.tong@gmail.com>
2018-07-13 21:21:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function placeOrder(cardNumber) {
     return  "Sorry, we don't have a credit card on file for you."
   }
   else{
-    var total = total()
+    var carttotal = total()
     cart.splice(0)
-    return `Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${carttotal}, which will be charged to the card ${cardNumber}.`
   }
 }
